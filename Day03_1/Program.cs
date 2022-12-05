c7f3621d45ab5b0cfc6037fd483ce97a7856c0da
Martin Zikmund <martin@zikmund.dev>
2022-12-04 20:35:08 -0500

    Day 3

---
+++ Day03_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read in the list of items for each rucksack.
+            string[] rucksacks = File.ReadAllLines("input.txt");
+
+            // Keep track of the sum of priorities.
+            int sum = 0;
+
+            // For each rucksack...
+            foreach (string rucksack in rucksacks)
+            {
+                // Find the first half and second half of the items in the rucksack.
+                string firstHalf = rucksack.Substring(0, rucksack.Length / 2);
+                string secondHalf = rucksack.Substring(rucksack.Length / 2);
+
+                // Find the item type that appears in both compartments.
+                char commonItemType = firstHalf.Intersect(secondHalf).Single();
+
+                // Calculate the priority of the item type.
+                int priority = (commonItemType >= 'a' && commonItemType <= 'z') ?
+                    commonItemType - 'a' + 1 :
+                    commonItemType - 'A' + 27;
+
+                // Add the priority to the sum.
+                sum += priority;
+            }
+
+            // Print the sum of priorities.
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Day03_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read in the list of items for each rucksack.
+            string[] rucksacks = File.ReadAllLines("input.txt");
+
+            // Keep track of the sum of priorities.
+            int sum = 0;
+
+            // Group the rucksacks into groups of three.
+            for (int i = 0; i < rucksacks.Length; i += 3)
+            {
+                string rucksack1 = rucksacks[i];
+                string rucksack2 = rucksacks[i + 1];
+                string rucksack3 = rucksacks[i + 2];
+
+                // Find the item type that appears in all three rucksacks.
+                char commonItemType =
+                    rucksack1.Intersect(rucksack2).Intersect(rucksack3).Single();
+
+                // Calculate the priority of the item type.
+                int priority = (commonItemType >= 'a' && commonItemType <= 'z') ?
+                    commonItemType - 'a' + 1 :
+                    commonItemType - 'A' + 27;
+
+                // Add the priority to the sum.
+                sum += priority;
+            }
+
+            // Print the sum of priorities.
+            Console.WriteLine(sum);
+        }
+    }
+}
