df8dbeba59ce63749f150347e5391b79edd9f5a1
Martin Zikmund <martin@zikmund.dev>
2022-12-04 19:50:08 -0500

    Day 1

---
+++ Day01_1/Program.cs
@@ -0,0 +1,36 @@
+﻿int currentElf = 1;
+int maxElf = 1;
+int currentCalories = 0;
+int maxCalories = 0;
+
+string line;
+
+// open the input file
+using (StreamReader reader = new StreamReader("input.txt"))
+{
+    // read each line of the input file
+    while ((line = reader.ReadLine()) != null)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            // start a new Elf's inventory
+            currentElf++;
+            currentCalories = 0;
+        }
+        else
+        {
+            // add the number of Calories to the current Elf's inventory
+            currentCalories += int.Parse(line);
+
+            // update the Elf with the most Calories
+            if (currentCalories > maxCalories)
+            {
+                maxElf = currentElf;
+                maxCalories = currentCalories;
+            }
+        }
+    }
+}
+
+// print the number of Calories carried by the Elf with the most Calories
+Console.WriteLine("Elf {0} has the most Calories with a total of {1}.", maxElf, maxCalories);
---
+++ Day01_2/Program.cs
@@ -0,0 +1,56 @@
+﻿int currentElf = 1;
+List<int> topElves = new List<int>();
+int currentCalories = 0;
+
+string line;
+
+// open the input file
+using (StreamReader reader = new StreamReader("input.txt"))
+{
+    // read each line of the input file
+    while ((line = reader.ReadLine()) != null)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            // start a new Elf's inventory
+
+            // add the current Elf's Calories to the top Elves
+            if (topElves.Count < 3)
+            {
+                topElves.Add(currentCalories);
+            }
+            else if (currentCalories > topElves.Min())
+            {
+                topElves[topElves.IndexOf(topElves.Min())] = currentCalories;
+            }
+
+            currentElf++;
+            currentCalories = 0;
+        }
+        else
+        {
+            // add the number of Calories to the current Elf's inventory
+            currentCalories += int.Parse(line);
+        }
+    }
+
+    // add the current Elf's Calories to the top Elves
+    if (topElves.Count < 3)
+    {
+        topElves.Add(currentCalories);
+    }
+    else if (currentCalories > topElves.Min())
+    {
+        topElves[topElves.IndexOf(topElves.Min())] = currentCalories;
+    }
+}
+
+// calculate the total number of Calories carried by the top three Elves
+int totalCalories = 0;
+foreach (int calories in topElves)
+{
+    totalCalories += calories;
+}
+
+// print the number of Calories carried by the top three Elves
+Console.WriteLine("The top three Elves have a total of {0} Calories.", totalCalories);
