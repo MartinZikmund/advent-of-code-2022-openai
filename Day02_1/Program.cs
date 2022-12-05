66a34f1ab1f4b8e502993835fd964d8a32d3b386
Martin Zikmund <martin@zikmund.dev>
2022-12-04 20:26:37 -0500

    Day 2

---
+++ Day02_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Solution
+{
+    public static void Main()
+    {
+        // Create a dictionary to store the scores for each shape
+        var scores = new Dictionary<string, int>();
+        scores["X"] = 1;
+        scores["Y"] = 2;
+        scores["Z"] = 3;
+
+        // Create a dictionary to store the outcome for each pair of shapes
+        var outcomes = new Dictionary<string, int>();
+        outcomes["AX"] = 3;
+        outcomes["AY"] = 6;
+        outcomes["AZ"] = 0;
+        outcomes["BX"] = 0;
+        outcomes["BY"] = 3;
+        outcomes["BZ"] = 6;
+        outcomes["CX"] = 6;
+        outcomes["CY"] = 0;
+        outcomes["CZ"] = 3;
+
+        // Initialize the total score to 0
+        int totalScore = 0;
+
+        // Open the input file
+        using (var input = new StreamReader("input.txt"))
+        {
+            // Read the strategy guide until we reach the end of the input
+            string line = input.ReadLine();
+            while (line != null)
+            {
+                // Split the line into the opponent's shape and the player's shape
+                string[] shapes = line.Split(' ');
+                string opponentShape = shapes[0];
+                string myShape = shapes[1];
+
+                // Calculate the score for this round
+                int score = scores[myShape] + outcomes[opponentShape + myShape];
+
+                // Add the score to the total score
+                totalScore += score;
+
+                // Read the next line of the strategy guide
+                line = input.ReadLine();
+            }
+        }
+
+        // Print the total score
+        Console.WriteLine(totalScore);
+    }
+}
---
+++ Day02_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Solution
+{
+    public static void Main()
+    {
+        // Create a dictionary to store the outcome for each round
+        var scores = new Dictionary<string, int>();
+        scores["X"] = 0;
+        scores["Y"] = 3;
+        scores["Z"] = 6;
+
+        // Create a dictionary to store the score for each pair of shapes
+        var outcomes = new Dictionary<string, int>();
+        outcomes["AX"] = 3;
+        outcomes["AY"] = 1;
+        outcomes["AZ"] = 2;
+        outcomes["BX"] = 1;
+        outcomes["BY"] = 2;
+        outcomes["BZ"] = 3;
+        outcomes["CX"] = 2;
+        outcomes["CY"] = 3;
+        outcomes["CZ"] = 1;
+
+        // Initialize the total score to 0
+        int totalScore = 0;
+
+        // Open the input file
+        using (var input = new StreamReader("input.txt"))
+        {
+            // Read the strategy guide until we reach the end of the input
+            string line = input.ReadLine();
+            while (line != null)
+            {
+                // Split the line into the opponent's shape and the player's shape
+                string[] shapes = line.Split(' ');
+                string opponentShape = shapes[0];
+                string myShape = shapes[1];
+
+                // Calculate the score for this round
+                int score = scores[myShape] + outcomes[opponentShape + myShape];
+
+                // Add the score to the total score
+                totalScore += score;
+
+                // Read the next line of the strategy guide
+                line = input.ReadLine();
+            }
+        }
+
+        // Print the total score
+        Console.WriteLine(totalScore);
+    }
+}
