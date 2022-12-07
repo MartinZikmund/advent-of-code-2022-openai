61bcdbd28b196c403bea56478130ad04fd5bbcb5
Martin Zikmund <martin@zikmund.dev>
2022-12-06 20:20:27 -0500

    Day 6

---
+++ Day06_1/Program.cs
@@ -0,0 +1,25 @@
+﻿string datastream = File.ReadAllText("input.txt");
+
+Queue<char> queue = new Queue<char>();
+
+for (int i = 0; i < datastream.Length; i++)
+{
+    char c = datastream[i];
+
+    queue.Enqueue(c);
+
+    if (queue.Count > 4)
+    {
+        queue.Dequeue();
+    }
+
+    if (queue.Count == 4)
+    {
+        HashSet<char> set = new HashSet<char>(queue);
+        if (set.Count == 4)
+        {
+            Console.WriteLine(i + 1);
+            break;
+        }
+    }
+}
---
+++ Day06_2/Program.cs
@@ -0,0 +1,25 @@
+﻿string datastream = File.ReadAllText("input.txt");
+
+Queue<char> queue = new Queue<char>();
+
+for (int i = 0; i < datastream.Length; i++)
+{
+    char c = datastream[i];
+
+    queue.Enqueue(c);
+
+    if (queue.Count > 14)
+    {
+        queue.Dequeue();
+    }
+
+    if (queue.Count == 14)
+    {
+        HashSet<char> set = new HashSet<char>(queue);
+        if (set.Count == 14)
+        {
+            Console.WriteLine(i + 1);
+            break;
+        }
+    }
+}
